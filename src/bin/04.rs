aed94fbc63bf52415711c73c01a32893bfeffd5b
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2024-12-04 10:17:48 +0100

    Solve day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -41,7 +41,41 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let m = input.lines().map(|l| l.chars().collect_vec()).collect_vec();
+
+    const OFFSETS: &[&[(i32, i32)]] = &[
+        &[(-1, -1), (0, 0), (1, 1)],
+        &[(-1, 1), (0, 0), (1, -1)],
+        &[(1, 1), (0, 0), (-1, -1)],
+        &[(1, -1), (0, 0), (-1, 1)],
+    ];
+
+    let mut count = 0;
+    for (y, line) in m.iter().enumerate() {
+        for x in 0..line.len() {
+            let p = OFFSETS
+                .iter()
+                .map(|offsets| {
+                    offsets
+                        .iter()
+                        .filter_map(|(oy, ox)| {
+                            let ny = y as i32 + oy;
+                            let nx = x as i32 + ox;
+                            if ny.is_negative() || nx.is_negative() {
+                                return None;
+                            }
+                            m.get(ny as usize).and_then(|a| a.get(nx as usize))
+                        })
+                        .collect::<String>()
+                })
+                .collect_vec();
+            let c = p.iter().filter(|s| s.as_str() == "MAS").count();
+            if c == 2 {
+                count += 1;
+            }
+        }
+    }
+    Some(count)
 }
 
 #[cfg(test)]
@@ -57,6 +91,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(9));
     }
 }
