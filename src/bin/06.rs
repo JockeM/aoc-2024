a67221aa021522126dc6d37070554bce5360bed8
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2024-12-07 00:11:48 +0100

    Solve day 6 part 2
    Really ugly, but why be smart if computer fast

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -61,8 +61,75 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(visited.len() as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let height = input.lines().count() as i32;
+    let width = input.lines().next().map_or(0, |line| line.len() as i32);
+
+    let mut map = HashSet::new();
+    let mut start_position = (0, 0);
+
+    input.lines().enumerate().for_each(|(y, line)| {
+        line.chars().enumerate().for_each(|(x, c)| match c {
+            '#' => {
+                map.insert((x as i32, y as i32));
+            }
+            '^' => {
+                start_position = (x as i32, y as i32);
+            }
+            _ => {}
+        });
+    });
+
+    let mut count = 0;
+    for y in 0..=height {
+        for x in 0..=width {
+            if map.contains(&(x, y)) || start_position == (x, y) {
+                continue;
+            }
+
+            map.insert((x, y));
+
+            if test(start_position, width, height, &map) {
+                count += 1;
+            }
+
+            map.remove(&(x, y));
+        }
+    }
+
+    Some(count)
+}
+
+fn test(start_position: (i32, i32), width: i32, height: i32, map: &HashSet<(i32, i32)>) -> bool {
+    let mut current_position = start_position;
+    let mut current_direction = Direction::North;
+
+    // If we get out in 10_000 moves we are probably stuck
+    // this is probably the most horrible bruteforce way to do this
+    for _ in 0..10_000 {
+        let next_position = match current_direction {
+            Direction::North => (current_position.0, current_position.1 - 1),
+            Direction::East => (current_position.0 + 1, current_position.1),
+            Direction::South => (current_position.0, current_position.1 + 1),
+            Direction::West => (current_position.0 - 1, current_position.1),
+        };
+
+        if !(0..width).contains(&next_position.0) || !(0..height).contains(&next_position.1) {
+            return false;
+        }
+
+        if map.contains(&next_position) {
+            current_direction = match current_direction {
+                Direction::North => Direction::East,
+                Direction::East => Direction::South,
+                Direction::South => Direction::West,
+                Direction::West => Direction::North,
+            };
+        } else {
+            current_position = next_position;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -78,6 +145,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
