76bb8de21ba7d5236836278dde4ea20231197847
Joakim Malmberg <10110847+JockeM@users.noreply.github.com>
2024-12-08 14:03:56 +0100

    Refactor day 7
    Small performance improvements but still a "stupid" solution

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -2,44 +2,32 @@ use itertools::Itertools;
 
 advent_of_code::solution!(7);
 
-fn concat(a: u64, b: u64) -> Option<u64> {
-    let mut b_digits = 0;
-    let mut temp_b = b;
-    while temp_b > 0 {
-        temp_b /= 10;
-        b_digits += 1;
-    }
-    if b_digits == 0 {
-        b_digits = 1;
-    }
-    Some(a * 10_u64.pow(b_digits) + b)
+fn concat(a: u64, b: u64) -> u64 {
+    let b_digits = (b as f64).log10().floor() as u32 + 1;
+    a * 10_u64.pow(b_digits) + b
 }
 
-fn makes_sum<F>(values: &[u64], ops: &[F], final_sum: u64) -> Option<u64>
-where
-    F: Fn(u64, u64) -> Option<u64>,
-{
+fn can_sum(values: &[u64], ops: &[u8], final_sum: u64) -> bool {
     let mut sum = values[0];
 
-    for (i, &value) in values.iter().enumerate().skip(1) {
+    for i in 1..values.len() {
+        let value = values[i];
+        sum = match ops[i - 1] {
+            0 => sum + value,
+            1 => sum * value,
+            2 => concat(sum, value),
+            _ => unreachable!(),
+        };
+
         if sum > final_sum {
-            return None;
+            return false;
         }
-
-        sum = ops[i - 1](sum, value)?;
     }
-
-    (final_sum == sum).then_some(sum)
+    sum == final_sum
 }
 
-fn generate_combinations<T>(n: usize, choices: &[T]) -> Vec<Vec<T>>
-where
-    T: Clone,
-{
-    (0..n)
-        .map(|_| choices.to_vec())
-        .multi_cartesian_product()
-        .collect()
+fn generate_combinations(n: usize, choices: u8) -> impl Iterator<Item = Vec<u8>> {
+    (0..n).map(|_| 0..choices).multi_cartesian_product()
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -55,16 +43,14 @@ pub fn part_one(input: &str) -> Option<u64> {
         })
         .collect_vec();
 
-    let choices: Vec<fn(u64, u64) -> Option<u64>> = vec![u64::checked_add, u64::checked_mul];
-
     Some(
         data.iter()
-            .map(|(sum, values)| {
-                generate_combinations(values.len() - 1, &choices)
-                    .into_iter()
-                    .find_map(|ops| makes_sum(&values, &ops, *sum))
-                    .unwrap_or(0)
+            .filter(|(sum, values)| {
+                generate_combinations(values.len() - 1, 2)
+                    .find(|ops| can_sum(values, &ops, *sum))
+                    .is_some()
             })
+            .map(|(sum, _)| sum)
             .sum::<u64>(),
     )
 }
@@ -82,17 +68,14 @@ pub fn part_two(input: &str) -> Option<u64> {
         })
         .collect_vec();
 
-    let choices: Vec<fn(u64, u64) -> Option<u64>> =
-        vec![u64::checked_add, u64::checked_mul, concat];
-
     Some(
         data.iter()
-            .map(|(sum, values)| {
-                generate_combinations(values.len() - 1, &choices)
-                    .into_iter()
-                    .find_map(|ops| makes_sum(&values, &ops, *sum))
-                    .unwrap_or(0)
+            .filter(|(sum, values)| {
+                generate_combinations(values.len() - 1, 3)
+                    .find(|ops| can_sum(values, &ops, *sum))
+                    .is_some()
             })
+            .map(|(sum, _)| sum)
             .sum::<u64>(),
     )
 }
@@ -111,5 +94,18 @@ mod tests {
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(11387));
+    }
+
+    #[test]
+    fn test_generate_combinations() {
+        let result: Vec<Vec<u8>> = generate_combinations(2, 2).collect();
+        assert_eq!(result, vec![vec![0, 0], vec![0, 1], vec![1, 0], vec![1, 1]]);
+    }
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(concat(1, 2), 12);
+        assert_eq!(concat(12, 3), 123);
+        assert_eq!(concat(123, 4), 1234);
     }
 }
